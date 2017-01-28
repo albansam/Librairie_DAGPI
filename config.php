2aeb5c2d7583b90aaf076eb1b6c805be1147595a
Pierre-Yves Le Guern <pierreyves.lgn@outlook.fr>
2017-01-28 16:51:56 +0100

    First commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    // Database
+    'database' => [
+        'engine' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'library',
+        'user' => 'root',
+        'password' => ''
+    ],
+
+    // Administrator auth
+    'admin' => ['admin', 'password']
+];
